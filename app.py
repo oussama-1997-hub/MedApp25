5cdadf575004a01fac898f30cdd62708dd4638b9
SondesHammami <sondes.hammami@enicar.ucar.tn>
2025-05-05 12:36:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if name:
 
 
 # 1. Charger les données
-df = pd.read_excel("https://github.com/SondesHammami/MedApp25/blob/main/BD_ML_1target.xlsx", engine="openpyxl")
+df = pd.read_excel("https://raw.githubusercontent.com/SondesHammami/MedApp25/main/BD_ML_1target.xlsx", engine="openpyxl")
 print(df)
 
 # 2. Séparer X et y (la dernière colonne est la cible)
