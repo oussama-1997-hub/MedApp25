31c5059f5b2e1d88f41a839d4db19acab170519f
SondesHammami <sondes.hammami@enicar.ucar.tn>
2025-05-05 12:06:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+%%writefile app.py
+import streamlit as st
 
+st.title("Streamlit in Colab")
+st.write("Hello from Google Colab!")
+
+name = st.text_input("What's your name?")
+if name:
+    st.write(f"Hello, {name}!")
