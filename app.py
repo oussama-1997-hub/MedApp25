8a67f552f5a5a3ba1dd13ecaf3f9f1ae591e780a
oussama-1997-hub <57638718+oussama-1997-hub@users.noreply.github.com>
2025-07-04 09:14:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,16 +36,13 @@ st.markdown(
         border-radius: 0.5rem;
         margin-bottom: 1rem;
     }
-
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
     </style>
     """,
-    # Add custom CSS to hide the GitHub icon
-    hide_github_icon = """
-    #GithubIcon {
-      visibility: hidden;
-    }
-    """
-    st.markdown(hide_github_icon, unsafe_allow_html=True)
     unsafe_allow_html=True
 )
 # ─── HEADER ─────────────────────────────────────────────────────────────────────
